323ab964734ceda98342d238cc01022433ba3c5b
Vojtech Machacek <vmachacek@gmail.com>
2024-09-14 14:26:13 -0400

    simplify

--- BrowserFactory.cs
+++ BrowserFactory.cs
@@ -7,14 +7,9 @@ public static class BrowserFactory
 {
     public static async Task<IBrowser> OpenBrowser()
     {
-        Environment.SetEnvironmentVariable("PW_EXPERIMENTAL_SERVICE_WORKER_NETWORK_EVENTS", "1");
-
-
         var browserParams = new[] { "--start-maximized", "--auto-open-devtools-for-tabs" };
 
-
         var playwright = await Playwright.CreateAsync();
-
 
         return await playwright.Chromium.LaunchAsync(
             new BrowserTypeLaunchOptions()
@@ -25,42 +20,22 @@ public static class BrowserFactory
             });
     }
 
-    public static async Task<IPage> OpenNewPage(IBrowser browser, bool isMobile = false)
+    public static async Task<IPage> OpenNewPage(IBrowser browser)
     {
         var contextOpts = new BrowserNewContextOptions()
         {
             ViewportSize = ViewportSize.NoViewport,
             Permissions = new[] { "clipboard-read", "clipboard-write" },
         };
-        IBrowserContext context;
-        if (isMobile)
-        {
-            var playwright = await Playwright.CreateAsync();
-            var iphone13 = playwright.Devices["iPhone 13"];
-            context = await browser.NewContextAsync(iphone13);
-        }
-        else
-        {
-            context = await browser.NewContextAsync(contextOpts);
-        }
-
-
-        var page = await context.NewPageAsync();
-
-        // this will get executed by each page load - when browser executes
-        await page.AddInitScriptAsync("""
-                                      function addNoAnimationsClass() { document.querySelector('html').classList.add('no-animations');}
-                                      const times = [1, 10, 50, 100,200,300,400,500,600,700];
-                                      for (let i = 0; i < times.length; i++) { setTimeout(addNoAnimationsClass, times[i]); }
-                                      """);
+        
+        var page = await (await browser.NewContextAsync(contextOpts)).NewPageAsync();
 
         await page.ExposeFunctionAsync("reportErrorToTestRunner", (object error, string url) =>
         {
             // here I want to fail the test
             return "";
         });
-
-
+        
         return page;
     }
 }
